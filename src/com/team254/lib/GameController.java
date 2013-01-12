d369b9403656839f9f11fc02f27b4e6a21dc51e3
Dorian Chan <articgrayling8x8@gmail.com>
2013-01-12 14:59:18 -0800

    Fixed commenting and indentation

--- src/com/team254/lib/GameController.java
+++ src/com/team254/lib/GameController.java
@@ -1,86 +1,124 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.team254.lib;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
- *
+ * Contains functions for use with the Logitech controller.
  * @author Dorian Chan, Kevin Vincent
  */
 public class GameController extends Joystick {
-    // Gamepad axis
-    private static final int kGamepadAxisLeftStickX = 1;
-    private static final int kGamepadAxisLeftStickY = 2;
-    private static final int kGamepadAxisShoulder = 3;
-    private static final int kGamepadAxisRightStickX = 4;
-    private static final int kGamepadAxisRightStickY = 5;
-    private static final int kGamepadAxisDpad = 6;
+  // Gamepad axis ports
+  private static final int kGamepadAxisLeftStickX = 1;
+  private static final int kGamepadAxisLeftStickY = 2;
+  private static final int kGamepadAxisShoulder = 3;
+  private static final int kGamepadAxisRightStickX = 4;
+  private static final int kGamepadAxisRightStickY = 5;
+  private static final int kGamepadAxisDpad = 6;
 
-    // Gamepad buttons
-    private static final int kGamepadButtonA = 1; // Bottom Button
-    private static final int kGamepadButtonB = 2; // Right Button
-    private static final int kGamepadButtonX = 3; // Left Button
-    private static final int kGamepadButtonY = 4; // Top Button
-    private static final int kGamepadButtonShoulderL = 5;
-    private static final int kGamepadButtonShoulderR = 6;
-    private static final int kGamepadButtonBack = 7;
-    private static final int kGamepadButtonStart = 8;
-    private static final int kGamepadButtonLeftStick = 9;
-    private static final int kGamepadButtonRightStick = 10;
-    private static final int kGamepadButtonMode = -1;
-    private static final int kGamepadButtonLogitech = -1;
-    
-    //Gamepad Itself!
-    public GameController(int gamepadPort) {
-        super(gamepadPort);
-    }
+  // Gamepad button ports
+  private static final int kGamepadButtonA = 1; // Bottom Button
+  private static final int kGamepadButtonB = 2; // Right Button
+  private static final int kGamepadButtonX = 3; // Left Button
+  private static final int kGamepadButtonY = 4; // Top Button
+  private static final int kGamepadButtonShoulderL = 5;
+  private static final int kGamepadButtonShoulderR = 6;
+  private static final int kGamepadButtonBack = 7;
+  private static final int kGamepadButtonStart = 8;
+  private static final int kGamepadButtonLeftStick = 9;
+  private static final int kGamepadButtonRightStick = 10;
+  private static final int kGamepadButtonMode = -1;
+  private static final int kGamepadButtonLogitech = -1;
+  
+  /**
+   * Constructor that creates a Joystick object.
+	*/
+  public GameController(int gamepadPort) {
+    super(gamepadPort);
+  }
 
-    //Sticks
-    public double getLeftX() {
-        return getRawAxis(kGamepadAxisLeftStickX);
-    }
+  /**
+   * Returns the X position of the left stick.
+	*/
+  public double getLeftX() {
+    return getRawAxis(kGamepadAxisLeftStickX);
+  }
+   
+  /**
+   * Returns the X position of the right stick.
+	*/
+  public double getRightX() {
+    return getRawAxis(kGamepadAxisRightStickX);
+  }
 
-    public double getRightX() {
-        return getRawAxis(kGamepadAxisRightStickX);
-    }
+  /**
+   * Returns the Y position of the left stick.
+	*/
+  public double getLeftY() {
+    return getRawAxis(kGamepadAxisLeftStickY);
+  }
 
-    public double getLeftY() {
-        return getRawAxis(kGamepadAxisLeftStickY);
-    }
+  /**
+   * Returns the Y position of the right stick.
+	*/
+  public double getRightY(){
+    return getRawAxis(kGamepadAxisRightStickY);
+  }
 
-    public double getRightY(){
-        return getRawAxis(kGamepadAxisRightStickY);
-    }
+  /**
+   * Checks whether Button A is being pressed and returns true if it is.
+	*/
+  public boolean getButtonStateA() {
+    return getRawButton(kGamepadButtonA);
+  }
+  
+  /**
+   * Checks whether Button B is being pressed and returns true if it is.
+	*/
+  public boolean getButtonStateB() {
+    return getRawButton(kGamepadButtonB);
+  }
+  
+  /**
+   * Checks whether Button X is being pressed and returns true if it is.
+	*/
+  public boolean getButtonStateX() { 
+    return getRawButton(kGamepadButtonX);
+  }
+  
+  /**
+   * Checks whether Button Y is being pressed and returns true if it is.
+	*/
+  public boolean getButtonStateY() {
+    return getRawButton(kGamepadButtonY);
+  }
 
-    //Buttons
-    public boolean getButtonStateA() {
-        return getRawButton(kGamepadButtonA);
-    }
-    public boolean getButtonStateB() {
-        return getRawButton(kGamepadButtonB);
-    }
-    public boolean getButtonStateX() { 
-        return getRawButton(kGamepadButtonX);
-    }
-    public boolean getButtonStateY() {
-        return getRawButton(kGamepadButtonY);
-    }
-
-    //Buttons
-    public JoystickButton getButtonA() {
-        return new JoystickButton(this, kGamepadButtonA);
-    }
-    public JoystickButton getButtonB() {
-        return new JoystickButton(this, kGamepadButtonB);
-    }
-    public JoystickButton getButtonX() {
-        return new JoystickButton(this, kGamepadButtonX);
-    }
-    public JoystickButton getButtonY() {
-        return new JoystickButton(this, kGamepadButtonY);
-    }
+  
+  /**
+   * Returns an object of Button A.
+	*/
+  public JoystickButton getButtonA() {
+    return new JoystickButton(this, kGamepadButtonA);
+  }
+  
+  /**
+   * Returns an object of Button B.
+	*/
+  public JoystickButton getButtonB() {
+    return new JoystickButton(this, kGamepadButtonB);
+  }
+  
+  /**
+   * Returns an object of Button X.
+	*/
+  public JoystickButton getButtonX() {
+    return new JoystickButton(this, kGamepadButtonX);
+  }
+  
+  /**
+   * Returns an object of Button Y.
+	*/
+  public JoystickButton getButtonY() {
+    return new JoystickButton(this, kGamepadButtonY);
+  }
 }
--- src/com/team254/lib/Util.java
+++ src/com/team254/lib/Util.java
@@ -1,17 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.team254.lib;
 
 /**
- *
+ * Contains basic functions that are used often.
  * @author Richard
  */
 public class Util {
-    
-    public static double limit(double v, double limit) {
-        return Math.abs(v) < limit ? v : limit * (v < 0? -1 : 1);
-    }
+  /**
+   * Checks whether a given number is within the given limit.
+   */
+  public static double limit(double v, double limit) {
+    return Math.abs(v) < limit ? v : limit * (v < 0? -1 : 1);
+  }
     
 }
