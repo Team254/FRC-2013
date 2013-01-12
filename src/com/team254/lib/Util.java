9d161d399817e1420a14a07e65b849ba5e11be14
Dorian Chan <articgrayling8x8@gmail.com>
2013-01-12 14:00:57 -0800

    Added Functions.java and explicit Button getters

--- src/com/team254/lib/Util.java
+++ src/com/team254/lib/Util.java
@@ -14,20 +14,4 @@ public class Util {
         return Math.abs(v) < limit ? v : limit * (v < 0? -1 : 1);
     }
     
-    // Sine Wave
-    public static double SineWave(double timeSec, double periodSec, double amplitude) {
-        return Math.sin(timeSec * 2 * Math.PI / periodSec) * amplitude;
-    }
-    
-    // Square Wave
-    public static double SquareWave(double timeSec, double periodSec, double amplitude) {
-        int periodMs = (int)(periodSec * 1000);
-        int incrementTimeMs = (int)(timeSec * 1000) % periodMs;
-        if (incrementTimeMs < periodMs / 2) {
-            return amplitude;
-        } 
-        else {
-            return -amplitude;
-        }
-    }
 }
