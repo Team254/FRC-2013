8271d30c4bc8728c969fbb39babbf90617700d76
Pinkerton <stephen@spinkerton.org>
2013-01-15 20:24:15 -0800

    Updated AutoModeSelector

--- src/com/team254/frc2013/AutoModeSelector.java
+++ src/com/team254/frc2013/AutoModeSelector.java
@@ -4,10 +4,38 @@
  */
 package com.team254.frc2013;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import java.util.Vector;
+
 /**
  *
  * @author tombot
+ * @author Arthur Kalb
+ * @author Stephen Pinkerton
  */
 public class AutoModeSelector {
-
+  Vector autoModes;
+  int index;
+  /**
+   * Adds all the autonomous modes 
+   */
+  public AutoModeSelector(){
+    index = 0;
+    //Add all the possible automodes
+  }
+  /**
+   * When the button is hit, the select mode will increment, or reset to 0
+   */
+  public void increment(){
+    index++;
+    if(index >= autoModes.size()){
+      index = 0;
+    }
+  }
+  /**
+   * gets the autnomous mode 
+   */
+  public CommandGroup getAutoMode(){
+    return ((CommandGroup)(autoModes.elementAt(index)));
+  }
 }
