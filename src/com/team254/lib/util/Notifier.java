623bb3f113493bb016cf4cc08d6df38b2b4c090a
Pinkerton <stephen@spinkerton.org>
2013-02-19 17:29:16 -0800

    Added toInteger method to allow for int type params in Notifier

--- src/com/team254/lib/util/Notifier.java
+++ src/com/team254/lib/util/Notifier.java
@@ -6,6 +6,8 @@ import java.util.Vector;
 /**
  * Controls all message subscriptions for Listeners.
  * Also allows Listeners to subscribe and unsubscribe from messages.
+ * Methods call for int type param instead of Integer because it's probably more 
+ * convenient for the person interfacing with this code.
  *
  * @author stephen@team254.com (Stephen Pinkerton)
  * @author art.kalb96@gmail.com (Arthur Kalb)
@@ -35,11 +37,11 @@ public class Notifier {
    * @param key: the hash table key that points to a vector of listeners
    * @param value: the value of the message
    */
-  public static void publish(Integer key, double value) {
-    Vector v = (Vector)getInstance().listeners.get(key);
+  public static void publish(int key, double value) {
+    Vector v = (Vector)getInstance().listeners.get(toInteger(key));
     if(v != null){
       for (int i = 0; i < v.size(); i++) {
-        ((Listener)(v.elementAt(i))).receive(key.intValue(),value);
+        ((Listener)(v.elementAt(i))).receive(key, value);
       }
     }
   }
@@ -49,11 +51,12 @@ public class Notifier {
    * @param key: message key to subscribe the listener to
    * @param listener: the listener to subscribe to the message key
    */
-  public static void subscribe(Integer key, Listener listener) {
-    if (getInstance().listeners.get(key) == null) {
-      getInstance().listeners.put(key, new Vector());
+  public static void subscribe(int key, Listener listener) {
+    Integer hashKey = toInteger(key);
+    if (getInstance().listeners.get(hashKey) == null) {
+      getInstance().listeners.put(hashKey, new Vector());
     }
-    ((Vector)getInstance().listeners.get(key)).addElement(listener);
+    ((Vector)getInstance().listeners.get(hashKey)).addElement(listener);
   }
 
   /**
@@ -61,10 +64,19 @@ public class Notifier {
    * @param key: message key to unsubscribe the listener from
    * @param listener: the listener being unsubscribed from the message key
    */
-  public static void unsubscribe(Integer key, Listener listener){
-    Vector v = (Vector)getInstance().listeners.get(key);
+  public static void unsubscribe(int key, Listener listener){
+    Vector v = (Vector)getInstance().listeners.get(toInteger(key));
     if(v != null) {
       v.removeElement(listener);
     }
+  }
+
+  /**
+   * Create and return an Integer object created from native int data type
+   * @param key: the int key to be turned into an object
+   * @return Integer (object) representation of key
+   */
+  private static Integer toInteger(int key) {
+    return new Integer(key);
   }
 }
