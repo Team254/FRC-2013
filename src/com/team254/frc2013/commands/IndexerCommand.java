a0087d231a93521c41ed322df9ba29c6f5cc628f
Patrick Fairbank <pat@team254.com>
2013-03-02 17:16:55 -0800

    Integrated control loop on/off detection into IndexerCommand.

--- src/com/team254/frc2013/commands/IndexerCommand.java
+++ src/com/team254/frc2013/commands/IndexerCommand.java
@@ -6,9 +6,12 @@ import edu.wpi.first.wpilibj.Timer;
  * Grabs Frisbee from conveyor and loads it into shooter.
  *
  * @author jonathan.chang13@gmail.com (Jonathan Chang)
+ * @author pat@team254.com (Patrick Fairbank)
  */
 public class IndexerCommand extends CommandBase {
   private boolean up;
+  private boolean controlLoopsOn;
+  private boolean conveyorOn;
   private Timer conveyorTimer;
 
   public IndexerCommand(boolean up) {
@@ -17,25 +20,46 @@ public class IndexerCommand extends CommandBase {
   }
 
   protected void initialize() {
-    shooter.setIndexerUp(up);
-    if (up) {
+    conveyorOn = false;
+    conveyorTimer.stop();
+    conveyorTimer.reset();
+    controlLoopsOn = controlBoard.operatorJoystick.getControlLoopsSwitchState();
+    if (!up) {
+      setTimeout(3);
+      shooter.setIndexerUp(false);
+    }
+  }
+
+  protected void execute() {
+    if ((controlLoopsOn && !up && shooter.isIndexerDown() && shooter.isShooterBack() ||
+        !controlLoopsOn && up) && !conveyorOn) {
+      conveyorOn = true;
+      shooter.setIndexerUp(true);
       conveyor.setMotor(0.9);
       conveyorTimer.reset();
       conveyorTimer.start();
     }
   }
 
-  protected void execute() {
-  }
-
   protected boolean isFinished() {
-    return !up || conveyorTimer.get() > 0.5;
+    if (controlLoopsOn) {
+      return up || conveyorTimer.get() > 0.5 || isTimedOut();
+    } else {
+      return !up || conveyorTimer.get() > 0.5;
+    }
   }
 
   protected void end() {
     conveyor.setMotor(0);
+    if (controlLoopsOn) {
+      shooter.setIndexerUp(true);
+    }
   }
 
   protected void interrupted() {
+    conveyor.setMotor(0);
+    if (controlLoopsOn) {
+      shooter.setIndexerUp(true);
+    }
   }
 }
