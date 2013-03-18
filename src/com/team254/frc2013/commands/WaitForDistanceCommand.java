55df857c5c47fd058030786a8c14b0d4f4b57823
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2013-03-17 21:05:06 -0700

    Add WaitForDistanceCommand

---
+++ src/com/team254/frc2013/commands/WaitForDistanceCommand.java
@@ -0,0 +1,33 @@
+/*
+ * Waits for a certain distance to be driven.
+ */
+package com.team254.frc2013.commands;
+
+/**
+ *
+ * @author Tom Bottiglieri (tom@team254.com)
+ */
+public class WaitForDistanceCommand extends CommandBase {
+  private double distance;
+  public WaitForDistanceCommand(double distance, double timeout) {
+    this.distance = distance;
+    setTimeout(timeout);
+  }
+  protected void initialize() {
+    
+  }
+
+  protected void execute() {
+  }
+
+  protected boolean isFinished() {
+    return ((drive.getLeftEncoderDistance() + drive.getRightEncoderDistance()) / 2) > distance  || isTimedOut();
+  }
+
+  protected void end() {
+  }
+
+  protected void interrupted() {
+  }
+  
+}
