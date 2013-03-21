9ff1543a9c354adfa800f3e4bea2397ce3359872
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2013-03-21 15:26:10 -0700

    Add Logger class

---
+++ src/com/team254/lib/util/Logger.java
@@ -0,0 +1,58 @@
+/*
+ * Logs strings to a file
+ */
+package com.team254.lib.util;
+
+import com.sun.squawk.microedition.io.FileConnection;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import javax.microedition.io.Connector;
+
+/**
+ *
+ * @author tom@team254.com (Tom Bottiglieri)
+ */
+public class Logger {
+  String filename;
+  OutputStreamWriter w;
+  FileConnection c;
+
+  public Logger(String filename, boolean open) {
+    this.filename = filename;
+    if (open)
+      open();
+  }
+
+  public Logger(String filename) {
+    this(filename, false);
+  }
+
+  public void open() {
+    String loc = "file:///" + filename;
+    try {
+      c = (FileConnection) Connector.open(loc);
+      OutputStreamWriter writer = new OutputStreamWriter(c.openOutputStream());
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void log(String s) {
+    if (w == null) {
+      return;
+    }
+    try {
+      w.write(s);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void close() {
+    try {
+      c.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
