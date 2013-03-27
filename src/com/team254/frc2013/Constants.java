96a4fade91ea0cb2b79b5c00023ad2ee31fafc9b
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2013-03-27 08:51:18 -0700

    Increase min shoot speed to match FTP'd file

--- src/com/team254/frc2013/Constants.java
+++ src/com/team254/frc2013/Constants.java
@@ -110,7 +110,7 @@ public class Constants extends ConstantsBase {
   public static final Constant intakeKI = new Constant("intakeKI", 0.0);
   public static final Constant intakeKD = new Constant("intakeKD", 0.24);
 
-  public static final Constant minShootRpm = new Constant("minShootRpm", 11000);
+  public static final Constant minShootRpm = new Constant("minShootRpm", 11750);
 
   static {
     // Set any overridden constants from the file on startup.
