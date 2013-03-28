99c290fb61e8f4012d80338545cddfb5e89d7af2
Patrick Fairbank <pat@team254.com>
2013-03-27 22:03:33 -0700

    Tweaked 7-disc auto some more.

--- src/com/team254/frc2013/auto/SevenDiscAutoMode.java
+++ src/com/team254/frc2013/auto/SevenDiscAutoMode.java
@@ -10,7 +10,6 @@ import com.team254.frc2013.commands.ShiftCommand;
 import com.team254.frc2013.commands.ShootSequenceCommand;
 import com.team254.frc2013.commands.ShooterOnCommand;
 import com.team254.frc2013.commands.ShooterPresetCommand;
-import com.team254.frc2013.commands.WaitCommand;
 import com.team254.frc2013.subsystems.Shooter;
 import com.team254.lib.control.impl.CustomProfile;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -30,33 +29,29 @@ public class SevenDiscAutoMode extends CommandGroup {
     addSequential(new ShooterOnCommand(true));
     addSequential(new ShooterPresetCommand(Shooter.PRESET_BACK_PYRAMID));
     addSequential(new IntakeRaiseCommand(IntakeRaiseCommand.INTAKE_DOWN));
-    addSequential(new WaitCommand(0.2));
+    addSequential(new ShiftCommand(false));
     addSequential(new ShootSequenceCommand());
     addSequential(new ShootSequenceCommand());
     addSequential(new ShootSequenceCommand());
 
     // Pick up 2 middle discs
-    addSequential(new ShiftCommand(false));
     addSequential(new ResetDriveEncodersCommand());
     addSequential(new ResetGyroCommand());
     addSequential(new RunIntakeCommand(1));
     addSequential(new ShooterPresetCommand(Shooter.PRESET_FRONT_PYRAMID));
-    addSequential(new DriveAtSpeedCommand(3.0, 2.0, 0, 3));
 
     // Drive to front of pyramid and shoot 2
-    addSequential(new DriveProfiledCommand(6, 3.5, 2.5));
-    addSequential(new WaitCommand(.25));
+    addSequential(new DriveProfiledCommand(6, 3, 5));
+    addSequential(new RunIntakeCommand(0.0));
     addSequential(new ShootSequenceCommand());
     addSequential(new ShootSequenceCommand());
 
     // Pick up 2 far discs
     addSequential(new RunIntakeCommand(1.0));
-    addSequential(new DriveAtSpeedCommand(10.0, 2.0, 0, 3));
-    addSequential(new WaitCommand(.25));
+    addSequential(new DriveAtSpeedCommand(10.5, 2.0, 0, 5));
 
     // Drive to front of pyramid and shoot 2
-    addSequential(new DriveProfiledCommand(6, 5, 2.3));
-    addSequential(new WaitCommand(.25));
+    addSequential(new DriveProfiledCommand(6, 5, 5));
     addSequential(new RunIntakeCommand(0.0));
     addSequential(new ShootSequenceCommand());
     addSequential(new ShootSequenceCommand());
