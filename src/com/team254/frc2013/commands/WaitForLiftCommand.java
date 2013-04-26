2f5ec76e939ce7b3dee13c549aa72b46328df028
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2013-04-26 09:23:14 -0700

    make intake shake for hang

--- src/com/team254/frc2013/commands/WaitForLiftCommand.java
+++ src/com/team254/frc2013/commands/WaitForLiftCommand.java
@@ -1,5 +1,7 @@
 package com.team254.frc2013.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+
 /**
  * Checks the pitch of the robot and waits for it to be swinging, indicating that the robot is off
  * the ground.
@@ -7,14 +9,23 @@ package com.team254.frc2013.commands;
  * @author pat@team254.com (Patrick Fairbank)
  */
 public class WaitForLiftCommand extends CommandBase {
+  Timer t = new Timer();
+  boolean up = true;
   public WaitForLiftCommand() {
   }
   protected void initialize() {
+    t.reset();
+    t.start();
     drive.shift(true);
   }
 
   protected void execute() {
     System.out.println("Waiting for lift...");
+    if (t.get() > .5) {
+      up = !up;
+    }
+    sc.wantIntakeUp = up;
+    sc.wantIntakeDown = !up;
   }
 
   protected boolean isFinished() {
@@ -23,6 +34,8 @@ public class WaitForLiftCommand extends CommandBase {
 
   protected void end() {
     System.out.println("Off the ground; okay for hanger extension.");
+        sc.wantIntakeUp = false;
+    sc.wantIntakeDown = false;
   }
 
   protected void interrupted() {
