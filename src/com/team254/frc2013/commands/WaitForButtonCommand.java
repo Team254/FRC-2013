f58d052f534fef6c0f9a0937706f6349088fe832
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2013-04-27 05:17:23 -0700

    Add WaitForButtonCommand

---
+++ src/com/team254/frc2013/commands/WaitForButtonCommand.java
@@ -0,0 +1,32 @@
+package com.team254.frc2013.commands;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author tom@team254.com (Tom Bottiglieri)
+ */
+public class WaitForButtonCommand extends Command{
+  Button b;
+  public WaitForButtonCommand(Button b) {
+    this.b = b;
+  }
+
+  protected void initialize() {
+  }
+
+  protected void execute() {
+  }
+
+  protected boolean isFinished() {
+    return b.get();
+  }
+
+  protected void end() {
+  }
+
+  protected void interrupted() {
+  }
+
+}
